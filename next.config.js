df96b95e203db599aabe530d5be0a787d0a436a2
Austin Fowler <dominantdidit@Mac.attlocal.net>
2024-12-11 16:22:32 -0800

    Fix: Convert next.config.ts to next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['items-images-production.s3.us-west-2.amazonaws.com']
+  }
+};
+
+module.exports = nextConfig;
--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  images: {
-    domains: ['items-images-production.s3.us-west-2.amazonaws.com']
-  }
-};
-
-export default nextConfig;
